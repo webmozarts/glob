6f3a05770fbf68322c1e6aa04e0d5600f0d72f94
Bernhard Schussek <bschussek@gmail.com>
2015-01-12 12:03:46 +0100

    Improved PhpDoc of the Glob class

--- src/Glob.php
+++ src/Glob.php
@@ -16,6 +16,55 @@ use Webmozart\Glob\Iterator\GlobIterator;
 /**
  * Utility methods for handling globs.
  *
+ * This class implements a Git-lik version of PHP's `glob()` function. The
+ * wildcard "*" matches any number of characters, *including* directory
+ * separators.
+ *
+ * Use {@link glob()} to glob the filesystem for paths:
+ *
+ * ```php
+ * foreach (Glob::glob('/project/*.twig') as $path) {
+ *     // do something...
+ * }
+ * ```
+ *
+ * Use {@link match()} to match a file path against a glob:
+ *
+ * ```php
+ * if (Glob::match('/project/views/index.html.twig', '/project/*.twig')) {
+ *     // path matches
+ * }
+ * ```
+ *
+ * You can also filter an array of paths for all paths that match your glob with
+ * {@link filter()}:
+ *
+ * ```php
+ * $filteredPaths = Glob::filter($paths, '/project/*.twig');
+ * ```
+ *
+ * Internally, the methods described above convert the glob into a regular
+ * expression that is then matched against the matched paths. If you need to
+ * match many paths against the same glob, you should convert the glob manually
+ * and use {@link preg_match()} to test the paths:
+ *
+ * ```php
+ * $staticPrefix = Glob::getStaticPrefix('/project/*.twig');
+ * $regEx = Glob::toRegEx('/project/*.twig');
+ *
+ * if (0 !== strpos($path, $staticPrefix)) {
+ *     // no match
+ * }
+ *
+ * if (!preg_match($regEx, $path)) {
+ *     // no match
+ * }
+ * ```
+ *
+ * The method {@link getStaticPrefix()} returns the part of the glob up to the
+ * first wildcard "*". You should always test whether a path has this prefix
+ * before calling the much more expensive {@link preg_match()}.
+ *
  * @since  1.0
  * @author Bernhard Schussek <bschussek@gmail.com>
  */
@@ -44,11 +93,25 @@ class Glob
     const E_STAR = '\\\\\\*';
 
     /**
-     * Globs the file system.
+     * Globs the file system paths matching the glob.
      *
-     * @param string $glob The glob.
+     * The glob may contain the wildcard "*". This wildcard matches any number
+     * of characters, *including* directory separators.
      *
-     * @return string[] The matching paths.
+     * ```php
+     * foreach (Glob::glob('/project/*.twig') as $path) {
+     *     // do something...
+     * }
+     * ```
+     *
+     * @param string $glob The canonical glob. The glob should contain forward
+     *                     slashes as directory separators only. It must not
+     *                     contain any "." or ".." segments. Use the
+     *                     "webmozart/path-util" utility to canonicalize globs
+     *                     prior to calling this method.
+     *
+     * @return string[] The matching paths. The keys of the array are
+     *                  incrementing integers.
      */
     public static function glob($glob)
     {
@@ -60,9 +123,160 @@ class Glob
     }
 
     /**
+     * Matches a path against a glob.
+     *
+     * ```php
+     * if (Glob::match('/project/views/index.html.twig', '/project/*.twig')) {
+     *     // path matches
+     * }
+     * ```
+     *
+     * @param string $path The path to match.
+     * @param string $glob The canonical glob. The glob should contain forward
+     *                     slashes as directory separators only. It must not
+     *                     contain any "." or ".." segments. Use the
+     *                     "webmozart/path-util" utility to canonicalize globs
+     *                     prior to calling this method.
+     *
+     * @return bool Returns `true` if the path is matched by the glob.
+     */
+    public static function match($path, $glob)
+    {
+        if (false === strpos($glob, '*')) {
+            return $glob === $path;
+        }
+
+        if (0 !== strpos($path, self::getStaticPrefix($glob))) {
+            return false;
+        }
+
+        if (!preg_match(self::toRegEx($glob), $path)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Filters an array for paths matching a glob.
+     *
+     * The filtered array is returned. This array preserves the keys of the
+     * passed array.
+     *
+     * ```php
+     * $filteredPaths = Glob::filter($paths, '/project/*.twig');
+     * ```
+     *
+     * @param string[] $paths A list of paths.
+     * @param string   $glob  The canonical glob. The glob should contain
+     *                        forward slashes as directory separators only. It
+     *                        must not contain any "." or ".." segments. Use the
+     *                        "webmozart/path-util" utility to canonicalize
+     *                        globs prior to calling this method.
+     *
+     * @return string[] The paths matching the glob indexed by their original
+     *                  keys.
+     */
+    public static function filter(array $paths, $glob)
+    {
+        if (false === strpos($glob, '*')) {
+            return in_array($glob, $paths) ? array($glob) : array();
+        }
+
+        $staticPrefix = self::getStaticPrefix($glob);
+        $regExp = self::toRegEx($glob);
+
+        return array_filter($paths, function ($path) use ($staticPrefix, $regExp) {
+            return 0 === strpos($path, $staticPrefix) && preg_match($regExp, $path);
+        });
+    }
+
+    /**
+     * Returns the base path of a glob.
+     *
+     * This method returns the most specific directory that contains all files
+     * matched by the glob. If this directory does not exist on the file system,
+     * it's not necessary to execute the glob algorithm.
+     *
+     * More specifically, the "base path" is the longest path trailed by a "/"
+     * on the left of the first wildcard "*". If the glob does not contain
+     * wildcards, the directory name of the glob is returned.
+     *
+     * ```php
+     * Glob::getBasePath('/css/*.css');
+     * // => /css
+     *
+     * Glob::getBasePath('/css/style.css');
+     * // => /css
+     *
+     * Glob::getBasePath('/css/st*.css');
+     * // => /css
+     *
+     * Glob::getBasePath('/*.css');
+     * // => /
+     * ```
+     *
+     * @param string $glob The canonical glob. The glob should contain forward
+     *                     slashes as directory separators only. It must not
+     *                     contain any "." or ".." segments. Use the
+     *                     "webmozart/path-util" utility to canonicalize globs
+     *                     prior to calling this method.
+     *
+     * @return string The base path of the glob.
+     */
+    public static function getBasePath($glob)
+    {
+        // Start searching for a "/" at the last character
+        $offset = -1;
+
+        // If the glob contains a wildcard "*", start searching for the
+        // "/" on the left of the wildcard
+        if (false !== ($pos = strpos($glob, '*'))) {
+            $offset = $pos - strlen($glob);
+        }
+
+        if (false !== ($pos = strrpos($glob, '/', $offset))) {
+            // Special case: Return "/" if the only slash is at the beginning
+            // of the glob
+            if (0 === $pos) {
+                return '/';
+            }
+
+            return substr($glob, 0, $pos);
+        }
+
+        // Glob contains no slashes on the left of the wildcard
+        // Return an empty string
+        return '';
+    }
+
+    /**
      * Converts a glob to a regular expression.
      *
-     * @param string $glob A path glob in canonical form.
+     * Use this method if you need to match many paths against a glob:
+     *
+     * ```php
+     * $staticPrefix = Glob::getStaticPrefix('/project/*.twig');
+     * $regEx = Glob::toRegEx('/project/*.twig');
+     *
+     * if (0 !== strpos($path, $staticPrefix)) {
+     *     // no match
+     * }
+     *
+     * if (!preg_match($regEx, $path)) {
+     *     // no match
+     * }
+     * ```
+     *
+     * You should always test whether a path contains the static prefix of the
+     * glob returned by {@link getStaticPrefix()} to reduce the number of calls
+     * to the expensive {@link preg_match()}.
+     *
+     * @param string $glob The canonical glob. The glob should contain forward
+     *                     slashes as directory separators only. It must not
+     *                     contain any "." or ".." segments. Use the
+     *                     "webmozart/path-util" utility to canonicalize globs
+     *                     prior to calling this method.
      *
      * @return string The regular expression for matching the glob.
      */
@@ -103,11 +317,14 @@ class Glob
     /**
      * Returns the static prefix of a glob.
      *
-     * The "static prefix" are all characters up to the first wildcard "*".
-     * If the glob does not contain wildcards, the full glob is
-     * returned.
+     * The "static prefix" is the part of the glob up to the first wildcard "*".
+     * If the glob does not contain wildcards, the full glob is returned.
      *
-     * @param string $glob A path glob in canonical form.
+     * @param string $glob The canonical glob. The glob should contain forward
+     *                     slashes as directory separators only. It must not
+     *                     contain any "." or ".." segments. Use the
+     *                     "webmozart/path-util" utility to canonicalize globs
+     *                     prior to calling this method.
      *
      * @return string The static prefix of the glob.
      */
@@ -118,104 +335,6 @@ class Glob
         }
 
         return $glob;
-    }
-
-    /**
-     * Returns the base path of a glob.
-     *
-     * The "base path" is the longest path trailed by a "/" on the left of the
-     * first wildcard "*". If the glob does not contain wildcards, the
-     * directory name of the glob is returned.
-     *
-     * ```php
-     * Glob::getBasePath('/css/*.css');
-     * // => /css
-     *
-     * Glob::getBasePath('/css/style.css');
-     * // => /css
-     *
-     * Glob::getBasePath('/css/st*.css');
-     * // => /css
-     *
-     * Glob::getBasePath('/*.css');
-     * // => /
-     * ```
-     *
-     * @param string $glob A path glob in canonical form.
-     *
-     * @return string The base path of the glob.
-     */
-    public static function getBasePath($glob)
-    {
-        // Start searching for a "/" at the last character
-        $offset = -1;
-
-        // If the glob contains a wildcard "*", start searching for the
-        // "/" on the left of the wildcard
-        if (false !== ($pos = strpos($glob, '*'))) {
-            $offset = $pos - strlen($glob);
-        }
-
-        if (false !== ($pos = strrpos($glob, '/', $offset))) {
-            // Special case: Return "/" if the only slash is at the beginning
-            // of the glob
-            if (0 === $pos) {
-                return '/';
-            }
-
-            return substr($glob, 0, $pos);
-        }
-
-        // Glob contains no slashes on the left of the wildcard
-        // Return an empty string
-        return '';
-    }
-
-    /**
-     * Matches a path against a glob.
-     *
-     * @param string $path The path.
-     * @param string $glob The glob.
-     *
-     * @return bool Returns `true` if the path is matched by the glob.
-     */
-    public static function match($path, $glob)
-    {
-        if (false === strpos($glob, '*')) {
-            return $glob === $path;
-        }
-
-        if (0 !== strpos($path, self::getStaticPrefix($glob))) {
-            return false;
-        }
-
-        if (!preg_match(self::toRegEx($glob), $path)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Filters paths matching a glob.
-     *
-     * @param string[] $paths A list of paths.
-     * @param string   $glob  The glob.
-     *
-     * @return string[] The paths matching the glob.
-     */
-    public static function filter(array $paths, $glob)
-    {
-        if (false === strpos($glob, '*')) {
-            return in_array($glob, $paths) ? array($glob) : array();
-        }
-
-        $staticPrefix = self::getStaticPrefix($glob);
-        $regExp = self::toRegEx($glob);
-
-        return array_filter($paths, function ($path) use ($staticPrefix, $regExp) {
-            return 0 === strpos($path, $staticPrefix) && preg_match($regExp, $path);
-        });
     }
 
     private function __construct()
